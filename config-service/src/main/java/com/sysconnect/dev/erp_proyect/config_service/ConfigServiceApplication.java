dec16b8bf399c34f595a176cebfd100ba56cb074
panteur <panteur@gmail.com>
2025-10-23 15:44:30 -0300

    commit config server eliminacion de imports no utilizados

--- config-service/src/main/java/com/sysconnect/dev/erp_proyect/config_service/ConfigServiceApplication.java
+++ config-service/src/main/java/com/sysconnect/dev/erp_proyect/config_service/ConfigServiceApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-import java.security.SecureRandom;
-
-import static com.sysconnect.dev.erp_proyect.config_service.utils.GeneradorCadenasAleatorias.generarCadenaAleatoria;
-
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServiceApplication {
